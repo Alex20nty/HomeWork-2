a9a3deff824a22af5bcdcc45842a537cc0b21253
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-20 04:38:43 +1100

    Add Exercise One

---
+++ ExerciseOne/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = new Random().Next(10, 100);
+Console.WriteLine(a);
+
+int b = a % 10;
+int c = a / 10;
+int max;
+
+if (b > c)
+{
+    max = b;
+}
+else
+{
+    max = c;
+}
+
+Console.WriteLine(max);
+
+
