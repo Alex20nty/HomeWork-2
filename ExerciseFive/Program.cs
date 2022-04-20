f24e054b57f59d24022530c5378243a3a7e9fd89
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-21 02:28:52 +1100

    Add Exercise Five

---
+++ ExerciseFive/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b==a*a)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
