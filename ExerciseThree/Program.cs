43d51e22ed40dac14e5079e1364a22ca4e1652e5
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-20 22:42:20 +1100

    Add Exercise Three

---
+++ ExerciseThree/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите два числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+int c = a % b;
+
+if(c == 0)
+{
+    Console.WriteLine(b+ " кратно "+a);
+}
+else
+{
+    Console.WriteLine(b+ " Не кратно "+a);
+    Console.WriteLine("Остаток "+c);
+}
+
+
