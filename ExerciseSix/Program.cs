2a57c129400495c1849485f42c7fa553aeb2bb7d
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-21 20:12:55 +1100

    Add Exercise Six

---
+++ ExerciseSix/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a>=100 && a<=999)
+{
+    int b = a / 10;
+    int c = b % 10;
+    Console.WriteLine(c);
+}
+else
+{
+    Console.WriteLine("ТРЕХзначное число тебе знакомо?");
+}
