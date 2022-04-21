57734e6d92792bc66ecf4bd9901f1bdfdcc52795
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-21 22:30:35 +1100

    Add Exercise Eight

---
+++ ExerciseEight/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a==1)
+{
+    Console.WriteLine("Monday - работаем");
+}
+    if(a==2)
+    {
+        Console.WriteLine("Tuesday - работаем");
+    }
+        if(a==3)
+        {
+            Console.WriteLine("Wednesday - работаем");
+        }
+            if(a==4)
+            {
+                Console.WriteLine("Thursday - работаем");
+            }
+                if(a==5)
+                {
+                    Console.WriteLine("Friday - еще работаем");
+                }
+                    if(a==6)
+                    {
+                        Console.WriteLine("Saturday - Ура выходной");
+                    }
+                        if(a==7)
+                        {
+                            Console.WriteLine("Sunday - Ура выходной");
+                        }
